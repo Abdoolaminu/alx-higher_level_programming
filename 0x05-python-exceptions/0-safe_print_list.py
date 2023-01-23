7c25e334b9b38323fda7b39c5b8835179b4261e8
Abdoolaminu <abdurrahmanminu2@gmail.com>
2023-01-23 11:09:08 +0000

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-
-def safe_print_list(my_list=[], x=0):
+afe_print_list(my_list=[], x=0):
 """Print x elememts of a list.
 
 Args:
@@ -19,4 +17,3 @@ except IndexError:
 break
 print("")
 return (ret)
-
