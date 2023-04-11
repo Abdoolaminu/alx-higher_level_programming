f358b7a0d6af2ec283fc3bf5ece09c8563481d65
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2023-04-11 22:24:28 +0000

    JavaScript

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -2,11 +2,11 @@
 rts = {
   /**
    * callMeMoby - Invokes a function t times.
-   * @param {Number} t - The number of times to invoke the function.
+   * @param {Number} x - The number of times to invoke the function.
    * @param {Number} theFunction - The function to be invoked.
    */
-  callMeMoby: function (t, theFunction) {
-    for (let i = 0; i < t; i++) {
+  callMeMoby: function (x, theFunction) {
+    for (let i = 0; i < x; i++) {
       theFunction();
     }
   }
