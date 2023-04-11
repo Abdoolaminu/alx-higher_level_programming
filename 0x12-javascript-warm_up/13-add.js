48b55092395e791581790aff63fcab774a72acca
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2023-04-11 22:12:43 +0000

    JavaScript

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.exports = {
+  /**
+   * sum - Sum of 2 numbers.
+   * @param {Number} x - The first number.
+   * @param {Number} y - The second number.
+   *
+   * @returns The sum of the 2 numbers.
+   */
+  add: (x, y) => x + y
+};
