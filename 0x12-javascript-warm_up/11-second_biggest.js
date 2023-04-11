67606bfc3bca3d5785ab792f6d99315b68c19b8a
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2023-04-11 22:06:51 +0000

    :JavaScript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const x = process.argv
   .slice(2)
-  .map(x => Number.parseInt(arg))
+  .map(arg => Number.parseInt(arg))
   .sort((a, b) => b - a);
-const i = args.length < 2 ? 0 : args[1];
+const i = x.length < 2 ? 0 : x[1];
 
 console.log(i);
