817cd5b17650a12e82abe17edb180a57d192347d
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2023-04-11 21:32:24 +0000

    JavaScript

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+ string T times
+ * @param {Number} t - The number of times to print the string.
+ * @param {String} str - The string to  print.
+ */
+function tPrint (t, str) {
+  if (Number.isNaN(t)) {
+    console.log('Missing number of occurrences');
+  } else if (t >= 0) {
+    for (let i = 0; i < t; i++) {
+      console.log(str);
+    }
+  }
+}
+
+tPrint(Number.parseInt(process.argv[2]), 'C is fun');
