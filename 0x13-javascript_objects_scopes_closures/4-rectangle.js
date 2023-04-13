73a9899372c890617de23ed0ec2146f0275b0426
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2023-04-13 02:29:36 +0000

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let m = 0; m < this.height; m++) {
+      let str = '';
+      for (let n = 0; n < this.width; n++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
