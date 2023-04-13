d11a4020fa3142bb2b18021de70d83b06e665296
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2023-04-13 02:37:04 +0000

    7-occurrences.js

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let nOc = 0;
+  for (let t = 0; t < list.length; t++) {
+    if (searchElement === list[i]) {
+      nOc++;
+    }
+  }
+  return nOc;
+};
