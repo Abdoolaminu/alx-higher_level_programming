3ad7e4d97b679ad031678741197b47c705871b36
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2023-04-27 15:57:14 -0700

    0-body_size.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -X GET "http://0.0.0.0:5000/catch_me" -H "User-Agent:Mozilla/5.0" -L -i -s -o /dev/null -w "HTTP %{http_code}\n" -H "Referer: http://google.com" -H "Origin: http://google.com" --data "You got me!"
+curl -X GET "http://0.0.0.0:5000/catch_me"  --data "You got me!"
 
