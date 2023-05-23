036f5cff5d862191f5c92b9f76904215e600a834
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2023-05-23 14:13:44 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,23 +1,11 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
-let count = 0;
-request(url, function (error, response, body) {
+const end = process.argv[2];
+
+request('https://swapi-api.hbtn.io/api/films/' + end, function (error, respones, body) {
   if (error) {
     console.log(error);
   } else {
-    const info = JSON.parse(body); // parse the body to JS object Notation
-    const info2 = info.results; // access to result information
-    for (let i = 0; i < info2.length; i++) {
-      if (info2[i].characters) { // if the characters information its available
-        for (const j of info2[i].characters) {
-          if (j.includes('18')) {
-            count++; // plus count if the ID its 18
-          }
-        }
-      }
-    }
-    console.log(count);
+    console.log(JSON.parse(body).title);
   }
 });
